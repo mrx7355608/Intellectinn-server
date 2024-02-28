028c722cd15261a299a38f5b84d923f52e0eb19b
mrx7355608 <mrx7355608@gmail.com>
2024-02-28 17:00:19 +0500

    renamed github repo

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,3 +1,8 @@
 import { Router } from "express";
+import { authControllers } from "../controllers/auth.controllers.js";
 
 const authRouter = Router();
+
+authRouter.post("/signup", authControllers.signup);
+
+export { authRouter };
