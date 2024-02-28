beef3a5b35308388887919374826d933af8f726f
mrx7355608 <mrx7355608@gmail.com>
2024-02-28 17:57:58 +0500

    created a validator for signup data

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,8 +1,10 @@
 import { ApiError } from "../utils/ApiError.js";
+import { signupValidator } from "../validators/auth.validators.js";
 
 export function AuthServices({ usersDB }) {
     const signup = async (data) => {
-        // TODO: Validate data
+        // Validate data
+        signupValidator(data);
 
         // Check if user exists
         const userExists = await usersDB.findUserByEmail(data.email);
@@ -13,8 +15,18 @@ export function AuthServices({ usersDB }) {
             );
         }
 
+        // Create user object with defualt values
+        const userObject = {
+            ...data,
+            profilePicture: process.env.DEFAULT_PROFILE_PICTURE,
+            followers: [],
+            following: [],
+            about: "",
+            googleId: null,
+        };
+
         // Save user in database
-        const newUser = await usersDB.insertNewUser(data);
+        const newUser = await usersDB.insertNewUser(userObject);
         return newUser;
     };
 
