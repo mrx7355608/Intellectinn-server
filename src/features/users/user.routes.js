ac9119c07ea7b19a8e3e122f7aa7077f23fa44da
mrx7355608 <mrx7355608@gmail.com>
2024-03-01 15:42:56 +0500

    just a missed commit

--- src/features/users/user.routes.js
+++ src/features/users/user.routes.js
@@ -3,8 +3,8 @@ import { userControllers } from "./user.controllers.js";
 
 const userRouter = Router();
 
-userRouter.get("/me", userControllers.getLoggedInUser)
-userRouter.get("/profile/:userID", userControllers.getUserProfile)
+userRouter.get("/me", userControllers.getLoggedInUser);
+userRouter.get("/profile/:userID", userControllers.getUserProfile);
 userRouter.patch("/update-account", userControllers.editUser);
 userRouter.delete("/delete-account", userControllers.deleteUser);
 userRouter.patch("/follow/:followingID", userControllers.followUser);
