518dd70d535ada8f0d25371ac26c12786e0194ba
mrx7355608 <mrx7355608@gmail.com>
2024-03-01 16:42:06 +0500

    added isAuth middleware for protected routes

--- src/features/users/user.routes.js
+++ src/features/users/user.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { userControllers } from "./user.controllers.js";
+import { isAuthenticated } from "../../middlewares/isAuthenticated.js";
 
 const userRouter = Router();
 
-userRouter.get("/me", userControllers.getLoggedInUser);
 userRouter.get("/profile/:userID", userControllers.getUserProfile);
+
+userRouter.use(isAuthenticated);
+userRouter.get("/me", userControllers.getLoggedInUser);
 userRouter.patch("/update-account", userControllers.editUser);
 userRouter.delete("/delete-account", userControllers.deleteUser);
 userRouter.patch("/follow/:followingID", userControllers.followUser);
