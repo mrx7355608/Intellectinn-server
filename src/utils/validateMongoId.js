692702741fd7c42899d1016ec8011724da160ec9
mrx7355608 <mrx7355608@gmail.com>
2024-03-03 14:55:12 +0500

    added a id type check

--- src/utils/validateMongoId.js
+++ src/utils/validateMongoId.js
@@ -2,6 +2,10 @@ import { ApiError } from "./ApiError.js";
 import validator from "validator";
 
 export function validateMongoID(id, entity) {
+    if (typeof id === "number") {
+        throw new ApiError(`Invalid ${entity} id`, 400);
+    }
+
     if (validator.isMongoId(id) === false) {
         throw new ApiError(`Invalid ${entity} id`, 400);
     }
