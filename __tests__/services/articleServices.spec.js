b64dd51a6dcab3368f720925c4298fc4d56e9033
mrx7355608 <mrx7355608@gmail.com>
2024-03-04 15:24:30 +0500

    refactored article services tests

--- __tests__/services/articleServices.spec.js
+++ __tests__/services/articleServices.spec.js
@@ -4,24 +4,13 @@ import jest from "jest-mock";
 const mockArticleWithAuthor = {
     _id: "65e44a2f701161ae7a5ff624",
     author: "65e44a401b2d7b2678969fca",
-};
-
-const mockArticleWithLikes = {
-    _id: "65e44a2f701161ae7a5ff624",
     likes: ["65e44a401b2d7b2678969fca"],
-};
-const mockArticleWithNoLikes = {
-    _id: "65e44a2f701161ae7a5ff624",
-    likes: [],
-};
-const mockPublishedArticle = {
-    _id: "65e44a2f701161ae7a5ff624",
-    author: "65e45dd6dba6a2185b189492",
     is_published: true,
 };
-const mockUnPublishedArticle = {
+const notPublishedArticle = {
     _id: "65e44a2f701161ae7a5ff624",
-    author: "65e45dd6dba6a2185b189492",
+    author: "65e44a401b2d7b2678969fca",
+    likes: ["65e44a401b2d7b2678969fca"],
     is_published: false,
 };
 
@@ -30,13 +19,9 @@ const articlesDB = {
     findById: jest
         .fn()
         .mockReturnValueOnce(null)
-        .mockReturnValueOnce(mockArticleWithAuthor)
-        .mockReturnValueOnce(mockArticleWithAuthor)
-        .mockReturnValueOnce(mockArticleWithLikes)
-        .mockReturnValueOnce(mockArticleWithNoLikes)
-        .mockReturnValueOnce(mockPublishedArticle)
-        .mockReturnValueOnce(mockPublishedArticle)
-        .mockReturnValue(mockUnPublishedArticle),
+        .mockReturnValueOnce(notPublishedArticle)
+        .mockReturnValueOnce(notPublishedArticle)
+        .mockReturnValue(mockArticleWithAuthor),
 };
 
 const articleServices = ArticleServices({ articlesDB });
@@ -63,6 +48,27 @@ describe("Article Services Tests", () => {
         });
     });
 
+    describe("Un-publish article", () => {
+        it("should throw error if user is not the author of the article", async () => {
+            try {
+                const articleID = "65e44a2f701161ae7a5ff624";
+                const userID = "65e59e9f7ce16a843cc98a1d";
+                await articleServices.unPublishArticle(articleID, userID);
+            } catch (err) {
+                expect(err.message).toBe("You cannot un-publish this article");
+            }
+        });
+        it("should throw error if article is already un-published", async () => {
+            try {
+                const articleID = "65e44a2f701161ae7a5ff624";
+                const userID = "65e44a401b2d7b2678969fca";
+                await articleServices.unPublishArticle(articleID, userID);
+            } catch (err) {
+                expect(err.message).toBe("Article has not been published yet");
+            }
+        });
+    });
+
     describe("Edit article", () => {
         it("should throw error if user is not the article's author", async () => {
             try {
@@ -78,7 +84,7 @@ describe("Article Services Tests", () => {
     describe("Delete article", () => {
         it("should throw error if user is not the article's author", async () => {
             try {
-                const userID = "65e44a2f701161ae7a5ff624";
+                const userID = "65e456094561d1b9ad011776";
                 const articleID = "65e448ba95c33ebb3a906aec";
                 await articleServices.removeArticle(articleID, userID);
             } catch (err) {
@@ -90,8 +96,8 @@ describe("Article Services Tests", () => {
     describe("Like article", () => {
         it("should throw error if user has already liked the article", async () => {
             try {
-                const articleID = "65e44a2f701161ae7a5ff624";
                 const userID = "65e44a401b2d7b2678969fca";
+                const articleID = "65e44a2f701161ae7a5ff624";
                 await articleServices.likeArticle(articleID, userID);
             } catch (err) {
                 expect(err.message).toBe("You have already liked this article");
@@ -102,8 +108,8 @@ describe("Article Services Tests", () => {
     describe("Unlike article", () => {
         it("should throw error if user has not liked the article yet", async () => {
             try {
+                const userID = "65e456094561d1b9ad011776";
                 const articleID = "65e44a2f701161ae7a5ff624";
-                const userID = "65e44a401b2d7b2678969fca";
                 await articleServices.unlikeArticle(articleID, userID);
             } catch (err) {
                 expect(err.message).toBe("You have not liked this article yet");
@@ -153,31 +159,10 @@ describe("Article Services Tests", () => {
         it("should throw error if article is already published", async () => {
             try {
                 const articleID = "65e44a2f701161ae7a5ff624";
-                const userID = "65e45dd6dba6a2185b189492";
+                const userID = "65e44a401b2d7b2678969fca";
                 await articleServices.publishArticle(articleID, userID);
             } catch (err) {
                 expect(err.message).toBe("Article is published already");
-            }
-        });
-    });
-
-    describe("Un-publish article", () => {
-        it("should throw error if user is not the author of the article", async () => {
-            try {
-                const articleID = "65e44a2f701161ae7a5ff624";
-                const userID = "65e44a401b2d7b2678969fca";
-                await articleServices.unPublishArticle(articleID, userID);
-            } catch (err) {
-                expect(err.message).toBe("You cannot un-publish this article");
-            }
-        });
-        it("should throw error if article is already un-published", async () => {
-            try {
-                const articleID = "65e44a2f701161ae7a5ff624";
-                const userID = "65e45dd6dba6a2185b189492";
-                await articleServices.unPublishArticle(articleID, userID);
-            } catch (err) {
-                expect(err.message).toBe("Article has not been published yet");
             }
         });
     });
