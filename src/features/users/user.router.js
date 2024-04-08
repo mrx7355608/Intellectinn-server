3e73ad0f12f6980a2356db604bfdfac4ddcab024
mrx7355608 <mrx7355608@gmail.com>
2024-04-08 20:01:02 +0500

    updated routes order

--- src/features/users/user.router.js
+++ src/features/users/user.router.js
@@ -5,8 +5,8 @@ import { isAuthenticated } from "../../middlewares/isAuthenticated.js";
 const userRouter = Router();
 
 userRouter.get("/me", isAuthenticated, userControllers.getLoggedInUser);
-userRouter.get("/:userID", userControllers.getUserProfile);
 userRouter.get("/search", userControllers.searchUsers);
+userRouter.get("/:userID", userControllers.getUserProfile);
 userRouter.get("/followers/:userID", userControllers.getUserFollowers);
 userRouter.get("/following/:userID", userControllers.getUserFollowings);
 
