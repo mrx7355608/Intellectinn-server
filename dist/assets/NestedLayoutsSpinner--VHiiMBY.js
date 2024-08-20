458a3a2217e798d1692afa164673f10bb713cee1
mrx7355608 <mrx7355608@gmail.com>
2024-08-20 17:53:37 +0500

    re-built react app

---
+++ dist/assets/NestedLayoutsSpinner--VHiiMBY.js
@@ -0,0 +1,1 @@
+import{j as e,B as t,S as n}from"./index-CdFokdOI.js";function r(){return e.jsx(t,{display:"flex",alignItems:"center",justifyContent:"center",w:"50vw",h:"20vh",mx:"auto",children:e.jsx(n,{})})}export{r as N};
